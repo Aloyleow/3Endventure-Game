6b4a52aef07adee98321db0dcd9af92bc064c3cf
Aloy <aloyleow91@gmail.com>
2024-07-22 14:47:01 +0800

    newcom

--- ThingsThatWork/rpg2.js
+++ ThingsThatWork/rpg2.js
@@ -11,11 +11,13 @@ const enemyRoll = document.querySelector(".enemyNumber");
 const playerHealth = document.querySelector("#playerHealth");
 const enemyHealth = document.querySelector("#enemyHealth");
 
+let charSelect = []
+let char ;
 let rpg = {
+    charText:[],
     storyText:[],
     deathText:[],
     buttonsEffect: {
-        charSelect: [],
         trainingSelect: [],
         pathSelect: [],
         roll: [],
@@ -34,20 +36,25 @@ let rpg = {
 }
 //PUSH TEXT FUNCTIONS
 function pushText(){
-    rpg.storyText.push("To be or not to pee, so who you wan to bee");
-    rpg.storyText.push("HUMAN !!!! What do you wan to do ?");
+    rpg.charText.push("To be or not to pee, so who you wan to bee");
+    rpg.charText.push("HUMAN !!!! What do you wan to do ?");
+    rpg.charText.push("KNIGHT !!! what you wanna slay ?");
+    rpg.charText.push("MAGE !!! tiu tiu tiu...")
+    
     rpg.storyText.push("Squash Squash kill that thing");
+    rpg.storyText.push("Slash Slaush ? Wad de Slush");
+    rpg.storyText.push("no mana or banana ?");
+    rpg.storyText.push("just a slime....chill..what you gonna do next ?");
 
     rpg.deathText.push("YOU DIED!!!! killed by a slime...lol...noob");
 }
-
-
 
 function pushPathSelectButtons() {
     rpg.buttonsEffect.pathSelect.push(
         "Fight a Slimeeee",
         "Fight a Dragon!!!",
         "Train with GOW!",
+        "Fight the GOW ?",
         "Chill...",
         "Chill lahh....",
         "Fight Anomaly",
@@ -56,7 +63,7 @@ function pushPathSelectButtons() {
 }
 
 function pushCharSelectButtons(){  
-    rpg.buttonsEffect.charSelect.push(
+    charSelect.push(
         "Human",
         "Knight!",
         "Mage"
@@ -93,10 +100,14 @@ function rng(){
 
 //BUTTON FUNCTIONS
 function charSelectButtons() {
-    button1.textContent = rpg.buttonsEffect.charSelect[0];
-    button2.textContent = rpg.buttonsEffect.charSelect[1];
-    button3.textContent = rpg.buttonsEffect.charSelect[2];
-    button1.addEventListener("click", humanSelectPage)
+    button1.textContent = charSelect[0];
+    button2.textContent = charSelect[1];
+    button3.textContent = charSelect[2];
+    button1.addEventListener("onclick", () =>
+        {humanPage);
+    button2.addEventListener("onclick", knightPage);
+    button3.addEventListener("onclick", magePage);
+    
 }
 
 function humanSelectButtons() {
@@ -106,7 +117,26 @@ function humanSelectButtons() {
     button1.addEventListener("click", fightSlimePage);
 
 }
-function rollButton() {
+
+function kmSelectButtons() {
+    button1.textContent = rpg.buttonsEffect.pathSelect[0];
+    button2.textContent = rpg.buttonsEffect.pathSelect[1];
+    button3.textContent = rpg.buttonsEffect.pathSelect[2];
+    button1.addEventListener("click", fightSlimePage)
+}
+
+function humanSelectButtonsAftSlime() {
+    button1.textContent = rpg.buttonsEffect.pathSelect[3];
+    button2.textContent = rpg.buttonsEffect.pathSelect[1];
+    button3.textContent = rpg.buttonsEffect.pathSelect[5];
+}
+
+function fightPageThings() {
+    playerRoll.style.visibility = "visible";
+    enemyRoll.style.visibility = "visible";
+    button1.style.visibility = "hidden";
+    button3.style.visibility = "hidden";
+    button2.textContent = "Roll";
     button2.addEventListener("click", () =>{
         rpg.playerScore = rng();
         rpg.enemyScore = rng();
@@ -114,16 +144,31 @@ function rollButton() {
         let y = rpg.enemyScore;
         playerRoll.textContent = x
         enemyRoll.textContent = y
+        winCondSlime()
     })
 
 }
 
-function winCond() {
-    if(rpg.playerScore <= rpg.enemyScore){
-        content.textContent = rpg.deathText[0];
-    } else {
-        console.log("lol");
+function winCondSlime() {
+    if (char === 0) {
+        if(rpg.playerScore <= rpg.enemyScore){
+            content.textContent = rpg.deathText[0];
+            button2.style.visibility = "hidden"
+
+        } else {
+            content.textContent = rpg.storyText[1];
+            humanSelectButtonsAftSlime();
+            showButtons();
+        }
     }
+    // if(rpg.playerScore <= rpg.enemyScore){
+    //     content.textContent = rpg.deathText[0];
+    //     button2.style.visibility = "hidden";
+    // } else {
+    //     content.textContent = rpg.storyText[1];
+    //     humanSelectButtonsAftSlime();
+    //     showButtons();
+    // }
 }
 
 
@@ -136,30 +181,47 @@ function hideScoreHealth() {
     enemyHealth.style.visibility = "hidden";
 }
 
-function fightRollDisplay() {
-    playerRoll.style.visibility = "visible";
-    enemyRoll.style.visibility = "visible";
-    button1.style.visibility = "hidden";
-    button3.style.visibility = "hidden";
-    button2.textContent = "Roll";
-    rollButton()
+function showButtons() {
+    button1.style.visibility = "visible";
+    button2.style.visibility ="visible";
+    button3.style.visibility = "visible";
 }
+
 
 //PAGE SELECT FUNCTIONS
 
 function charSelectPage(){
-    content.textContent = rpg.storyText[0];
+    content.textContent = rpg.charText[0];
     charSelectButtons();
 }
 
-function humanSelectPage(){
-    content.textContent = rpg.storyText[1];
+function humanPage(){
+    content.textContent = rpg.charText[1];
     humanSelectButtons()
 }
 
+function knightPage(){
+    content.textContent = rpg.charText[2];
+    kmSelectButtons();
+}
+
+function magePage(){
+    content.textContent = rpg.charText[3];
+    kmSelectButtons();
+}
+
 function fightSlimePage(){
-    content.textContent = rpg.storyText[2];
-    fightRollDisplay()
+    if (char === 0){
+       content.textContent = rpg.storyText[0];
+       fightPageThings()
+    } else if (char === 1){
+       content.textContent = rpg.storyText[1];
+       fightPageThings()
+    } else if (char === 2){
+       content.textContent = rpg.storyText[2];
+       fightPageThings()
+    }
+    
 }
 
 
