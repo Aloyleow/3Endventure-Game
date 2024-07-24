d02a67c94fff5d0d9e41534cd5f843296f54a4bd
Aloy <aloyleow@gmail.com>
2024-07-24 21:57:21 +0800

    final

--- ThingsThatWork/rpg4.js
+++ ThingsThatWork/rpg4.js
@@ -18,10 +18,6 @@ const enemyHealthDisplay = document.querySelector("#enemyHealth");
 let rpg = {
     chosenChar: "",
 
-    secondFight: false,
-    thirdFight: false,
-    bossFight: false,
-
     slimeFight: false,
     dragonFight: false,
     humanDeathbyChilling: false,
@@ -48,9 +44,8 @@ let rpgfightSystem = {
     health: {
         anomalyHealth: 50,
         playerHealthHuman: 35,
-        playerHealthDragonLord: 50,
-        playerHealthaftTrain: 50,
-        playerHealthGOW: 100,
+        playerHealthKnight: 45,
+        playerHealthMAge: 40
     } 
 }
 function rng(){
@@ -94,72 +89,73 @@ function pushCharTextButtons(){
 }
 
 function pushStoryContent(){    
-    rpg.storyText.push("0Squash Squash kill that thing");
-    rpg.storyText.push("1Slash Slaush ? Wad de Slush");
-    rpg.storyText.push("2no mana or banana ?");
-    rpg.storyText.push("3just a slime....chill..what you gonna do next ?");
-    rpg.storyText.push("4slimey slimey slashed!! what you gonna do next ?");
-    rpg.storyText.push("5VAPOURISED!! what you gonna do next ?");
-    rpg.storyText.push("6FIGHTTT!! FOR YOUR LIFEEEE");
-    rpg.storyText.push("7Slice those chonky thic legs");
-    rpg.storyText.push("8BANG! POW! , wait, no, tiu tiu tiu!!!");
-    rpg.storyText.push("9WELL DONE human!!!, you practically ascended to dragon lord after getting dragon blood on your face.");
-    rpg.storyText.push("10Chonky lizard dieded....What you gonna do next ?");
-    rpg.storyText.push("11Train to kill!!");
-    rpg.storyText.push("12WOW chonky thihck muscles");
-    rpg.storyText.push("1 quadrillion push up13");
-    rpg.storyText.push("1st plate...bruh......why this option thoo14");
-    rpg.storyText.push("errr weelll....you full ?15");
-    rpg.storyText.push("1 quadrillion sit ups16");
-    rpg.storyText.push("17After killing the slime you feel like a sigma ? ok lorr you try");
-    rpg.storyText.push("18Well...this wasnt supposed to happen...but now go  and KILL the ANOMALY with your attained powers!")
+    rpg.storyText.push("Squash Squash kill that thing!");
+    rpg.storyText.push("Slash Slaush ? Wad de Slush");
+    rpg.storyText.push("No mana or banana ?");
+    rpg.storyText.push("Slimey puddle.....FIGHT THE ANOMALY!!");
+    rpg.storyText.push("Slimey slimey slashed!! FIGHT THE ANOMALY!!");
+    rpg.storyText.push("VAPOURISED!! FIGHT THE ANOMALY!!");
+    rpg.storyText.push("FIGHTTT!! FOR YOUR LIFEEEE");
+    rpg.storyText.push("Slice those chonky thic legs");
+    rpg.storyText.push("BANG! POW! , wait, no, tiu tiu tiu!!!");
+    rpg.storyText.push("You killed the Anomaly!!! Well done!!");
+    rpg.storyText.push("Chonky lizard dieded....FIGHT THE ANOMALY!!");
+    rpg.storyText.push("Train to kill!!");
+    rpg.storyText.push("WOW chonky thihck muscles...FIGHT THE ANOMALY!!");
+    rpg.storyText.push("1 quadrillion push up");
+    rpg.storyText.push("1 quadrillion plate...bruh......why this option thoo");
+    rpg.storyText.push("errr weelll....you full ?");
+    rpg.storyText.push("1 quadrillion sit ups");
+    rpg.storyText.push("HOW ARE YOU STILL HUNGRY ?? you practically ate up the anomaly!! congrats ?");
+    rpg.storyText.push("ROLLL!!!")
 
-    rpg.deathText.push("0YOU DIED!!!! killed by a slime...lol...noob");
-    rpg.deathText.push("1YOU DIED!!!! obviously...dragon leh");
-    rpg.deathText.push("2YOU DIED!!!! your next journey will be through the dragons other end =)")
-    rpg.deathText.push("3YOU DIED !!!..off old age..Yawnzz");
-    rpg.deathText.push("4YOU DIED!!!! err.....you weak lor");
-    rpg.deathText.push("5YOU DIED!!!! yeaaaaaaa i mean thats alot of rice");
-    rpg.deathText.push("6YOU DIED!!!! ah fight a god lahh....orbigood");
+    rpg.deathText.push("YOU DIED!!!! killed by a slime...lol...noob");
+    rpg.deathText.push("YOU DIED!!!! obviously...dragon leh");
+    rpg.deathText.push("YOU DIED!!!! your next journey will be through the dragons other end =)")
+    rpg.deathText.push("YOU DIED !!!..off old age..Yawnzz");
+    rpg.deathText.push("YOU DIED!!!! your muscle spasm until you died.......");
+    rpg.deathText.push("YOU DIED!!!! yeaaaaaaa i mean thats alot of rice");
+    rpg.deathText.push("YOU DIED!!!! who's gonna save the world now ??!! ahhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh");
 
 }
 
 function pushPathSelectButtons() {
     rpg.buttonsEffect.pathSelect.push(
-        "0Fight a Slimeeee",
-        "1Fight a Dragon!!!",
-        "2Train with GOW!",
-        "3Fight the GOW ?",
-        "4Chill...",
-        "5Chill lahh....",
-        "6Fight Anomaly",
-        "7Roll",
-        "8Chill lahh...with a teh ping",
-        "9Do a Quadrillion push ups!!",
-        "10Do a Quadrillion sit ups!!",
-        "11Eat a Quadrillion plate of ZAP CAI PENG with neker gen neker gen neker ling curry zi hai you ji dan",
+        "Fight a Slimeeee",
+        "Fight a Dragon!!!",
+        "Train with GOW!",
+        "Fight the GOW ?",
+        "Chill...",
+        "Chill lahh....",
+        "Fight Anomaly",
+        "Roll",
+        "Chill lahh...with a teh ping",
+        "Do a Quadrillion push ups!!",
+        "Do a Quadrillion sit ups!!",
+        "Eat a Quadrillion plate of ZAP CAI PENG with neker gen neker gen neker ling curry zi hai you ji dan",
+        "Fight The Anomaly!!!"
     )
 }
 
-function fightSystem(){
-    button2.addEventListener("click", () =>{
-        showScore()
-        rpgfightSystem.playerScore = rng();
-        rpgfightSystem.enemyScore = rng();
-        playerRollDisplay.textContent = rpgfightSystem.playerScore;
-        enemyRollDisplay.textContent = rpgfightSystem.enemyScore;
-        if (rpg.slimeFight === true){
-            slimeBossWinCond()
-        } else if (rpg.dragonFight === true){
-            dragonBossWinCond()
-        } else if (rpg.gowFight === true){
-            gowWinCon()
-        } else{
-            trainWinCond()
-        } 
-               
-    })
+function fightSystemSet(){
+    showScore()
+    rpgfightSystem.playerScore = rng();
+    rpgfightSystem.enemyScore = rng();
+    playerRollDisplay.textContent = rpgfightSystem.playerScore;
+    enemyRollDisplay.textContent = rpgfightSystem.enemyScore;
+    if (rpg.slimeFight === true){
+        slimeBossWinCond()
+    } else if (rpg.dragonFight === true){
+        dragonBossWinCond()
+    } else{
+        trainWinCond()
+    }   
+    
 }
+function fightSystem(){
+    button2.addEventListener("click", fightSystemSet);               
+}
+
 
 function humanPageButtons(){
     button1.textContent = rpg.buttonsEffect.pathSelect[0];
@@ -204,25 +200,9 @@ function removetrainWithGowButtons(){
     button3.removeEventListener("click", trainEatPage);
 }
 
-function humanPageButtonsAftSlime(){
-    
-    button1.textContent = rpg.buttonsEffect.pathSelect[3];
-    button2.textContent = rpg.buttonsEffect.pathSelect[1];
-    button3.textContent = rpg.buttonsEffect.pathSelect[8];
-    button1.addEventListener("click", fightGowPage);
-    button2.addEventListener("click", dragonBossFightPage);
-    button3.addEventListener("click", instantDeath);
-
-}
-
-function kmPageButtonsAftSlime(){
-    removeKmPageButtons()
-    button1.textContent = rpg.buttonsEffect.pathSelect[6];
-    button2.textContent = rpg.buttonsEffect.pathSelect[1]
-    button3.textContent = rpg.buttonsEffect.pathSelect[2];
-    button1.addEventListener
-    button2.addEventListener("click", dragonBossFightPage)
-    button3.addEventListener
+function removeButtonsforAnomaly(){
+    button2.removeEventListener("click", dragonBossFightPage);
+    button2.removeEventListener("click", fightSystemSet);
 }
 //================================================1st page Character Select================================================
 
@@ -244,6 +224,7 @@ function charSelectButtons(){
 //================================================2nd page Choose your way================================================
 
 function afterCharSelectPage(){
+
     hideCharSelectButtons();
     showBottomButtons();
     if (rpg.chosenChar === "human"){
@@ -283,9 +264,10 @@ function slimeBossWinCond(){
     if (rpg.chosenChar === "human"){
         if (rpgfightSystem.playerScore > rpgfightSystem.enemyScore) {
             content.textContent = rpg.storyText[3];
-            humanPageButtonsAftSlime();
-            showBottomButtons();
-            rpg.slimeFight = false;
+            anomalyFightPage()
+            
+           
+            
         } else {
             content.textContent = rpg.deathText[0];
             replayButton();
@@ -293,9 +275,9 @@ function slimeBossWinCond(){
     } else if (rpg.chosenChar === "knight"){
         if (rpgfightSystem.playerScore > rpgfightSystem.enemyScore) {
             content.textContent = rpg.storyText[4];
-            kmPageButtonsAftSlime();
-            showBottomButtons();
-            rpg.slimeFight = false;
+            anomalyFightPage()
+            
+            
         } else {
             content.textContent = rpg.deathText[0];
             replayButton();
@@ -303,9 +285,9 @@ function slimeBossWinCond(){
     } else if (rpg.chosenChar === "mage"){
         if (rpgfightSystem.playerScore > rpgfightSystem.enemyScore) {
             content.textContent = rpg.storyText[5];
-            kmPageButtonsAftSlime();
-            showBottomButtons();
-            rpg.slimeFight = false;
+            anomalyFightPage()
+            
+            
         } else {
             content.textContent = rpg.deathText[0];
             replayButton();
@@ -314,8 +296,8 @@ function slimeBossWinCond(){
 
 }
 // remember set false
-function dragonBossFightPage(){
-    rpg.dragonFight =true 
+function dragonBossFightPage(){ 
+    rpg.dragonFight =true
     onlyShowRollButton();
     hideScore()
     if (rpg.chosenChar === "human") {
@@ -335,15 +317,20 @@ function dragonBossWinCond(){
     if (rpg.chosenChar === "human"){
         if (rpgfightSystem.playerScore === 10) {
             content.textContent = rpg.storyText[9];
-            rpg.dragonFight = false;
+            anomalyFightPage();
+            
+            
         } else {
             content.textContent = rpg.deathText[1];
-            replayButton()
+            replayButton();
+            
         }        
     } else if (rpg.chosenChar === "knight"){
         if (rpgfightSystem.playerScore > rpgfightSystem.enemyScore) {
             content.textContent = rpg.storyText[10];
-            rpg.dragonFight = false;
+            anomalyFightPage();
+            
+            
         } else {
             content.textContent = rpg.deathText[2];
             replayButton()
@@ -351,7 +338,9 @@ function dragonBossWinCond(){
     } else if (rpg.chosenChar === "mage"){
         if (rpgfightSystem.playerScore > rpgfightSystem.enemyScore) {
             content.textContent = rpg.storyText[10];
-            rpg.dragonFight = false;
+            anomalyFightPage()
+            
+            
         } else {
             content.textContent = rpg.deathText[2];
             replayButton()
@@ -359,18 +348,60 @@ function dragonBossWinCond(){
     }
 }
 
-function fightGowPage(){
-    gowFight = true;
-    rpg.textContent = rpg.storyText[17];
-    fightSystem()
+//=======anomalyfunctions======
+
+function anomalyFightPage(){
+    removeButtonsforAnomaly();
+    anomalyButtonDisplay();
+    button2.textContent = rpg.buttonsEffect.pathSelect[12];
+    button2.addEventListener("click", () =>{
+        playerRollDisplay.textContent = 0;
+        enemyRollDisplay.textContent = 0;
+        playerHealthDisplay.textContent = rpgfightSystem.health.playerHealthKnight
+        enemyHealthDisplay.textContent = rpgfightSystem.health.anomalyHealth
+        playerHealthDisplay.style.visibility = "visible";
+        enemyHealthDisplay.style.visibility = "visible";
+        content.textContent = rpg.storyText[18]
+        button2.textContent = rpg.buttonsEffect.pathSelect[7];
+        rpgfightSystem.playerScore = rng()
+        rpgfightSystem.enemyScore = rng()
+        anomalyfightSystem()
+       })
+       
 }
 
-function gowWinCon(){
-    if (rpgfightSystem.playerScore === 10){
-        content.textContent = rpg.storyText[18];
-    } else {
-        content.textContent = rpg.deathText[6];
+
+function charHealth() {
+    if (rpg.chosenChar === "human"){        
+        playerHealthDisplay.textContent = rpgfightSystem.health.playerHealthHuman
+    }else if (rpg.chosenChar === "knight"){
+        playerHealthDisplay.textContent = rpgfightSystem.health.playerHealthKnight
+    }else if (rpg.chosenChar === "mage"){
+        playerHealthDisplay.textContent = rpgfightSystem.health.playerHealthMAge
     }
+
+}
+
+function anomalyfightSystem(){
+    button2.addEventListener("click", () =>{
+        
+        playerRollDisplay.textContent = rpgfightSystem.playerScore;
+        enemyRollDisplay.textContent = rpgfightSystem.enemyScore;
+        
+        rpgfightSystem.health.anomalyHealth = rpgfightSystem.health.anomalyHealth - rpgfightSystem.playerScore
+        rpgfightSystem.health.playerHealthKnight = rpgfightSystem.health.playerHealthKnight - rpgfightSystem.enemyScore
+
+        if (rpgfightSystem.health.anomalyHealth <= 0 && rpgfightSystem.health.anomalyHealth <= rpgfightSystem.health.playerHealthKnight){
+            replayButtonWin()
+            hideScoreAndHealth()
+            content.textContent = rpg.storyText[9];
+        } else if (rpgfightSystem.health.playerHealthKnight <= 0 && rpgfightSystem.health.playerHealthKnight <= rpgfightSystem.health.anomalyHealth){
+            replayButton()
+            hideScoreAndHealth()
+            content.textContent =rpg.deathText[6];
+        }
+        
+    }, {once: true});
 }
 
 //================================================ training page after choosing train type================================================
@@ -380,7 +411,6 @@ function trainPuPage(){
     removetrainWithGowButtons()
     onlyShowRollButton();
     content.textContent = rpg.storyText[13]
-
     fightSystem();
 }
 function trainSuPage(){
@@ -388,7 +418,6 @@ function trainSuPage(){
     removetrainWithGowButtons()
     onlyShowRollButton();
     content.textContent = rpg.storyText[16]
-
     fightSystem();
 }
 
@@ -397,7 +426,6 @@ function trainEatPage(){
     removetrainWithGowButtons()
     onlyShowRollButton();
     content.textContent = rpg.storyText[14]
-
     fightSystem();
 }
 
@@ -405,6 +433,8 @@ function trainWinCond(){
     if (rpg.trainingPU === true){
         if(rpgfightSystem.playerScore > rpgfightSystem.enemyScore){
             content.textContent = rpg.storyText[12]
+            anomalyFightPage()
+            
         }else{
             content.textContent = rpg.deathText[4];
             replayButton()
@@ -412,19 +442,26 @@ function trainWinCond(){
     } else if (rpg.trainingSU === true){
         if(rpgfightSystem.playerScore > rpgfightSystem.enemyScore){
             content.textContent = rpg.storyText[12]
+            anomalyFightPage()
+            
         }else{
             content.textContent = rpg.deathText[4];
             replayButton()
+            
         }
     } else if (rpg.trainingEat === true){
         if(rpgfightSystem.playerScore === 10){
             content.textContent = rpg.storyText[15]
+            replayButtonWinByEating()
+            
         }else{
             content.textContent = rpg.deathText[5];
             replayButton()
         }
     }
 }
+
+
 
 //================================================ Buttons Visibility================================================
 function hideScoreAndHealth() {
@@ -463,11 +500,25 @@ function hidebottomButtons() {
     button3.style.visibility = "hidden";
 }
 
+
 function onlyShowRollButton() {
     button2.textContent = rpg.buttonsEffect.pathSelect[7];
     button1.style.visibility = "hidden";
     button2.style.visibility ="visible";
     button3.style.visibility = "hidden";
+}
+
+function anomalyButtonDisplay(){
+    button1.style.visibility = "hidden";
+    button2.style.visibility ="visible";
+    button3.style.visibility = "hidden";
+}
+
+function replayButtonWin(){
+    hidebottomButtons()
+    knightbutton.style.visibility ="visible"
+    knightbutton.textContent = "Time for Lunch ?"
+    knightbutton.addEventListener("click", () => {history.go(0)})
 }
 
 function replayButton(){
@@ -477,22 +528,38 @@ function replayButton(){
     knightbutton.addEventListener("click", () => {history.go(0)})
 }
 
+function replayButtonWinByEating(){
+    button2.textContent = rpg.buttonsEffect.pathSelect[12];
+    button2.addEventListener("click", () => {
+        hideScore()
+        removeButtonsforAnomaly();
+        hidebottomButtons();
+        knightbutton.style.visibility = "visible";
+        knightbutton.textContent = "Time for Lunch ?";
+        knightbutton.addEventListener("click", () => { history.go(0) });
+        content.textContent = rpg.storyText[17];    
+    })
+    
+}
+    
+
+
 //================================================instant death functions================================================
 function instantDeath(){
     content.textContent = rpg.deathText[3];
-    replayButton()
+    replayButton();
 }
 
 function render(){
 
-    pushCharText()
-    pushCharTextButtons()
+    pushCharText();
+    pushCharTextButtons();
 
-    pushStoryContent()
-    pushPathSelectButtons()
+    pushStoryContent();
+    pushPathSelectButtons();
 
-    charSelectButtons()
+    charSelectButtons();
 
 }
 
-render()
+render();
